32b9e7a94f026043a2fa2fcaff4d97ebc8c35865
snappyboy16 <dreykfame@gmail.com>
2024-02-17 22:20:42 +0300

    .

---
+++ app/src/main/java/com/example/kursedapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kursedapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
