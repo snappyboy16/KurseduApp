45eebcd71f25d4e242fde7bbc3c1f4e9af46a65e
snappyboy16 <dreykfame@gmail.com>
2024-02-18 00:20:31 +0300

    +сделал переход на активти Auth через 3 секунды

--- app/src/main/java/com/example/kursedapp/MainActivity.kt
+++ app/src/main/java/com/example/kursedapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kursedapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            intentAuth()
+        }, 3000)
+    }
+    fun intentAuth() {
+        val intentActivityAuth = Intent(this, Auth::class.java)
+        startActivity(intentActivityAuth)
+        finish()
     }
 }
