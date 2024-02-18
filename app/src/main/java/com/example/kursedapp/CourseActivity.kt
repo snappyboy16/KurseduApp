2d7c5f8d2d854cc5b30f9ea93f5d74db80767044
snappyboy16 <dreykfame@gmail.com>
2024-02-18 16:37:22 +0300

    +добавил активити Курсы

---
+++ app/src/main/java/com/example/kursedapp/CourseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kursedapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course)
+    }
+}
