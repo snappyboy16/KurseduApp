862447b1eb3fb2702e927055ec7fc5f904fe4241
snappyboy16 <dreykfame@gmail.com>
2024-02-18 16:36:56 +0300

    +сделал тему свою +добавил шрифт AnonymousPro +сделал проверку на авторизованного пользователя

--- app/src/main/java/com/example/kursedapp/MainActivity.kt
+++ app/src/main/java/com/example/kursedapp/MainActivity.kt
@@ -3,20 +3,38 @@ package com.example.kursedapp
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Handler().postDelayed({
-            intentAuth()
-        }, 3000)
+        auth = Firebase.auth
+        if (auth.currentUser != null){
+            Handler().postDelayed({
+                Log.d("Account", "${auth.currentUser!!.email}")
+                intentCourse()
+            }, 3000)
+        } else {
+            Handler().postDelayed({
+                intentAuth()
+            }, 3000)
+        }
     }
     fun intentAuth() {
-        val intentActivityAuth = Intent(this, Auth::class.java)
-        startActivity(intentActivityAuth)
+        val intentActivityAuthActivity = Intent(this, AuthActivity::class.java)
+        startActivity(intentActivityAuthActivity)
+        finish()
+    }
+    private fun intentCourse() {
+        val intentActivityCourseActivity = Intent(this, CourseActivity::class.java)
+        startActivity(intentActivityCourseActivity)
         finish()
     }
 }
